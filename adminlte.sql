47c05fcff583a1c21a5307a8348d126e52280999
Juwendays <juwendays@gmail.com>
2020-05-18 09:09:15 +0700

    Commit second

--- adminlte.sql
+++ adminlte.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Mar 2020 pada 09.36
+-- Waktu pembuatan: 18 Bulan Mei 2020 pada 13.45
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 5.6.40
 
@@ -25,6 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` varchar(64) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'default.jpg',
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`product_id`, `name`, `price`, `image`, `description`) VALUES
+('5ec1c63645f7b', 'Kulkas', 2000000, '5ec1c63645f7b.png', 'Kulkas \r\n'),
+('5ec1c6a740cfb', 'Televisi 32 inch', 4000000, '5ec1c6a740cfb.png', 'Televisi 32 inch'),
+('5ec1c7776541a', 'MacBook', 15000000, '5ec1c7776541a.jpg', 'Macbook\r\n'),
+('5ec1ce6594624', 'Rice Cooker', 300000, '5ec1ce6594624.jpg', 'Rice Cocker'),
+('5ec1dee74ae39', 'Lampu LED', 80000, '5ec1dee74ae39.jpg', 'Lampu LED'),
+('5ec1dff846286', 'KIpas Angin', 150000, '5ec1dff846286.jpg', 'Kipas Angin');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `users`
 --
 
@@ -35,6 +61,13 @@ CREATE TABLE `users` (
   `username` varchar(32) NOT NULL,
   `password` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`) VALUES
+(1, 'Fahmi Ihza Nugroho', 'fahmiihza90@gmail.com', 'reinne', '170566afb86b37c41b3ead102e4d7315');
 
 --
 -- Indexes for dumped tables
@@ -55,7 +88,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
